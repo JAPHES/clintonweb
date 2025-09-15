7072664325d19d71ada1ac936fec9d429a775472
JAPHES <japhesmurithi>
2025-09-15 21:57:53 +0300

    fix static files settings

--- clintonweb/settings.py
+++ clintonweb/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, ""),
+    os.path.join(BASE_DIR, "clinton/static"),
 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')

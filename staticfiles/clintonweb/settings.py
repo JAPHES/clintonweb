403457a81d4d908200c2213a0b97cc812d90834f
JAPHES <japhesmurithi>
2025-09-15 21:38:14 +0300

    installed whitenoise

--- staticfiles/clintonweb/settings.py
+++ staticfiles/clintonweb/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -124,6 +125,10 @@ STATICFILES_DIRS = [
 
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+#whitenoise setting
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Default primary key field type
---
+++ staticfiles/staticfiles/clinton/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# the project urls
+
+urlpatterns = [
+    path('', views.home, name='home'),                  # Home Page
+    path('projects/', views.projects, name='projects'), # Projects Page
+    path('contact/', views.contact, name='contact'),    # Contact Me Page
+]

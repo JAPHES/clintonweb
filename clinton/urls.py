b795d6554d28d9b5c482122a50cb1dc977264db7
JAPHES <japhesmurithi@gmail.com>
2026-02-02 18:55:02 +0300

    made download cv work

--- clinton/urls.py
+++ clinton/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),                  # Home Page
     path('projects/', views.projects, name='projects'), # Projects Page
     path('contact/', views.contact, name='contact'),    # Contact Me Page
+    path('download-cv/', views.download_cv, name='download_cv'),
 ]
--- clinton/views.py
+++ clinton/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import FileResponse
+import os
+from django.conf import settings
+
+def download_cv(request):
+    filepath = os.path.join(settings.BASE_DIR, 'clinton/static/clinton/files/clintoncv.pdf')
+    return FileResponse(open(filepath, 'rb'), as_attachment=True, filename='clintoncv.pdf')
+
 
 # Create your views here.
 def home(request):
